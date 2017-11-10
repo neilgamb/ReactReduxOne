21de885167a3d206c6ef36c933e5b4dfdbf00519
neilgamb <neilson@goekos.com>
2017-11-10 16:46:41 -0500

    comments/notes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-//
+// TO DO:
+// 1. Create a new component.  This component should produce some HTML
+// 2. Take this component's generated HTML and render it to the DOM
+
+const App = function () {
+    return <div>Hi!</div>;
+}
+
+// const = like var but it will NOT change.  const is reserved for variables that 
+// are not meant to be reassigned.  App is not expected to change here.
+
+// '<div>Hi!</div>;' = JSX, a dialect / subset of JS.  Webpack and Babel will transpile 
+// JSX into vanilla JS. 
+// -> check out Babel compiler to test what JSX will be compiled into (https://babeljs.io/repl/)
+
+
+
+
+
