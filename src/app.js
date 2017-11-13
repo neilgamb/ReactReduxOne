fe862b2680381ef67ea863a00fb0532075411be6
neilgamb <neilson@goekos.com>
2017-11-13 14:14:26 -0500

    Basic app functionality added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
 import SearchBar from './components/search-bar';
-
+import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
+import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyCgJSb9wOBtoqynnb7TnA89AiTbaHiBncA';
 
-const YouTube = () => {
-    return <div>YouTube Clone</div>;
+class YouTube extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            selectedVideo: null,
+            videos: []
+        };
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => { // same as function(videos) {...} ES6
+            // this.setState({ videos: videos }); ES6 allows just:
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+                    videos={this.state.videos} />  {/* passing data is known as passing 'props'*/}
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<YouTube />, document.querySelector('.container'))
+
+
+// downwards data-flow: only the most parent components that needs data should be 
+// responsible for fetching data.
--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+// { Component } = const Component = React.Component // syntactic sugar
+
+// currently this is a functional or 'presentational' component.
+// because we need it to have some awareness (ie, hey, the user has typed something, do this with that input...)
+// we need to upgrade it to a class-based on 'container' component
+
+/*
 const SearchBar = () => {
     return <input />;
 };
+*/
+
+class SearchBar extends Component {  // see line 3 comments
+    constructor(props) {
+        super(props)
+
+        this.state = { term: '' }; // only time you use this syntax to set value of state! otherwise, use setState()
+    }
+    render() {  // what is run when a component is re-renders
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({ term: event.target.value })} />
+                Value of input: {this.state.term}
+            </div>
+        );
+    }
+}
 
 export default SearchBar;
 
-
+// state: a plain JS object used to RECORD and REACT to user events
+// each class-based component has its own state.  whenver its state changes, it 
+// automatically re-renders and forces all children to re-render.
